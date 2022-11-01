3deb8a1fc76f4bb42cf91bef88ba9be00b38c48f
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-01 21:11:31 +0300

    test main

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
-const github = require("@actions/github");
-const axios = require("axios");
-const { oauthToken, orgId, ticketName } = process.env;
-
-console.log(oauthToken.length, orgId.length, ticketName.length);
-console.log('Successfully');
+fetch(`https://api.tracker.yandex.net/v2/issues/${process.env.TICKET_ID}/comments`, {
+  method: "POST",
+  headers: {
+    Authorization: `OAuth ${process.env.AUTH_TOKEN}`,
+    "X-Org-ID": `${process.env.ORG_ID}`,
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ text: "Тест" }),
+});
