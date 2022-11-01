7d9c34a0734558bca1d5ef19552a7bf5946a9087
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-01 22:37:08 +0300

    ticket upd test

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,2 @@
-const { OUATH_TOKEN, TICKET_ID, X_ORG_ID } = process.env
-console.log('test', X_ORG_ID)
+const { TRACKER_OUATH_TOKEN, TICKET_ID, X_ORG_ID } = process.env
+console.log('test', TICKET_ID)
