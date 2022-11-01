d9d915570d611126ba9772d035803cbf0746af84
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-01 22:52:14 +0300

    actions and core installed

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
-const { TRACKER_OUATH_TOKEN, TICKET_ID, X_ORG_ID } = process.env
-console.log('test', TICKET_ID)
+import * as github from '@actions/github'
+const { TRACKER_OATH_TOKEN, TICKET_ID, X_ORG_ID } = process.env
+
+export async function updateTicket() {
+  const regexp = /rc-0.0.(<maintenance>\d+)/
+  console.log(regexp)
+  const tag = github.context.ref_name
+  console.log(tag)
+  const { maintenance } = tag.match(regexp)
+  console.log(maintenance)
+}
