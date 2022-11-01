b468f832f33d5595e90e0c2c8660be562745d1f8
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-01 21:31:06 +0300

    test main

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,1 @@
-fetch(`https://api.tracker.yandex.net/v2/issues/${process.env.TICKET_ID}/comments`, {
-  method: "POST",
-  headers: {
-    Authorization: `OAuth ${process.env.AUTH_TOKEN}`,
-    "X-Org-ID": `${process.env.ORG_ID}`,
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ text: "Тест" }),
-});
+const { TRACKER_OUATH_TOKEN, TICKET_ID, X_ORG_ID } = process.env
