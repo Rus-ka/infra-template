39216c4c58768f6576fb4ffe5488e52e4de09f21
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-02 01:29:50 +0300

    added updticket and image

--- scripts/image.js
+++ scripts/image.js
@@ -1,23 +1,28 @@
-const fetch = require("node-fetch");
-const exec = require("@actions/exec");
+import fetch from "node-fetch";
+import exec from "@actions/exec";
 
 const { TAG, TICKET_ID, AUTH_TOKEN, ORG_ID } = process.env;
 const headers = {
-  Authorization: `OAuth ${AUTH_TOKEN}`,
-  'X-Org-ID': ORG_ID,
+    Authorization: `OAuth ${AUTH_TOKEN}`,
+    'X-Org-ID': ORG_ID,
 };
 
 const main = async () => {
-  await exec.exec('docker', ['build', '-t', `app:${TAG}`, '.']);
-  console.log('Listen on port 3000');
+    try {
+        await exec.exec('docker', ['build', '-t', `app:${TAG}`, '.']);
+        console.log('Listen on port 3000');
 
-  await fetch(`https://api.tracker.yandex.net/v2/issues/${TICKET_ID}/comments`, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify({
-      text: `Собрали образ с тегом ${TAG}`
-    })
-  });
-}
+        await fetch(`https://api.tracker.yandex.net/v2/issues/${TICKET_ID}/comments`, {
+            method: 'POST',
+            headers,
+            body: JSON.stringify({
+            text: `Собрали образ с тегом ${TAG}`
+            })
+        });
+        console.log('Комментарий успешно создан')
+    } catch (e) {
+    console.error("Ошибка при создании комментария", e.message)
+    }
+};
 
 main().then(() => console.log('Comment added'));
