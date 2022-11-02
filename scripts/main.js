e13ab98822b3c20a7a2195e79e49f8959451a1d3
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-02 23:32:12 +0300

    fetch test

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 import { threadId } from "worker_threads";
-
+console.log(process.env.AUTH_TOKEN, process.env.X_ORG_ID, process.env.TICKET_ID)
 fetch(`https://api.tracker.yandex.net/v2/issues/${process.env.TICKET_ID}/comments`, {
   method: "POST",
   headers: {
@@ -9,5 +9,6 @@ fetch(`https://api.tracker.yandex.net/v2/issues/${process.env.TICKET_ID}/comment
     "Content-Type": "application/json",
   },
   body: JSON.stringify({ text: "Тест" }),
-})
+}).then((x) => console.log(x))
+
 
