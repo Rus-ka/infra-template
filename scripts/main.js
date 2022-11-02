005fbe397749937d7c864c554fbc9ea417cd5b56
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-03 00:00:34 +0300

    patch  test

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 import fetch from "node-fetch";
 import { threadId } from "worker_threads";
 console.log(process.env.AUTH_TOKEN, process.env.X_ORG_ID, process.env.TICKET_ID)
-fetch(`https://api.tracker.yandex.net/v2/issues/${process.env.TICKET_ID}/comments`, {
-  method: "POST",
+fetch(`https://api.tracker.yandex.net/v2/issues/${process.env.TICKET_ID}`, {
+  method: "PATCH",
   headers: {
     Authorization: `OAuth ${process.env.AUTH_TOKEN}`,
     "X-Org-ID": `${process.env.X_ORG_ID}`,
     "Content-Type": "application/json",
   },
-  body: JSON.stringify({ text: "Тест" }),
+  body: JSON.stringify({ 'Summary': "Кросс-проверка ИНФРА 03.11" }),
 }).then((x) => console.log(x))
 
 
