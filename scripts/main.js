09aedcfedaedb91815b01ff4e2e2794573a1d533
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-03 00:15:15 +0300

    added update ticket script

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 import fetch from "node-fetch";
-import { threadId } from "worker_threads";
-console.log(process.env.AUTH_TOKEN, process.env.X_ORG_ID, process.env.TICKET_ID)
+
 fetch(`https://api.tracker.yandex.net/v2/issues/${process.env.TICKET_ID}`, {
   method: "PATCH",
   headers: {
